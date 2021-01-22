8c97e67d60c4016ceada379947db3448690317f7
huguesfils <huguesfils@icloud.com>
2021-01-22 18:40:13 +0100

    capacitor deleted, cordova install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,26 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAmwnFnMefJ7BSKO6a1F2X3vxm9zNQpVC8",
+    authDomain: "task-manager-13216.firebaseapp.com",
+    databaseURL: "https://task-manager-13216-default-rtdb.firebaseio.com",
+    projectId: "task-manager-13216",
+    storageBucket: "task-manager-13216.appspot.com",
+    messagingSenderId: "400399678439",
+    appId: "1:400399678439:web:f018fe2ac4b3f2579b5b42",
+    measurementId: "G-ME0274T58N"
+}
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { PopoverController } from '@ionic/angular';
+import {PopoverComponent} from '../popover/popover.component';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,67 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  currentDate: string;
+  title: string;
+  description: string;
+  addTask: boolean;
+  tasks = [];
 
-  constructor() {}
+  constructor(public afDB: AngularFireDatabase, public popoverController: PopoverController) {
+    const date = new Date();
+    const options = { weekday: 'long', month: 'long', day: 'numeric' };
+    this.currentDate = date.toLocaleDateString('fr-FR', options);
+    this.getTasks();
+  }
+
+  async showHelp(ev: any) {
+    const popover = await this.popoverController.create({
+      component: PopoverComponent,
+      event: ev,
+      translucent: false
+    });
+    return await popover.present();
+  }
+
+  addTaskToFirebase() {
+    this.afDB.list('Tasks/').push({
+      title: this.title,
+      description: this.description,
+      date: new Date().toISOString(),
+    });
+    this.showForm();
+  }
+  
+  showForm() {
+    this.addTask = !this.addTask;
+    this.title = '';
+    this.description = '';
+  }
+
+  getTasks() {
+    this.afDB.list('Tasks/').snapshotChanges(['child_added', 'child_removed']).subscribe(actions => {
+      this.tasks = [];
+      actions.forEach(action => {
+        this.tasks.push({
+          key: action.key,
+          title: action.payload.exportVal().title,
+          description: action.payload.exportVal().description,
+          date: action.payload.exportVal().date.substring(11, 16),
+        });
+      });
+    });
+  }
+
+  deleteTask(task: any) {
+    this.afDB.list('Tasks/').remove(task.key);
+  }
+  editTask(task:any){
+    this.deleteTask(task);
+    this.showForm();
+    this.title = task.title;
+    this.description = task.description;
+    }
 
 }
+
+
