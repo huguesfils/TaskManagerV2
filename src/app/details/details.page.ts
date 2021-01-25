99f6b4e02cb919789899326c1b3bd9b5bdaf513b
huguesfils <huguesfils@icloud.com>
2021-01-25 15:50:51 +0100

    try to pass data between home and details

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController, NavParams } from '@ionic/angular';
+
 
 
 @Component({
@@ -10,8 +10,8 @@ import { NavController, NavParams } from '@ionic/angular';
 export class DetailsPage implements OnInit {
   selectedItem: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.selectedItem = navParams.get('tasks')
+  constructor() {
+    
    }
 
   ngOnInit() {
