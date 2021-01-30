d29a253e9d9b403c157203c6fc92c3bdcae812bc
huguesfils <huguesfils@icloud.com>
2021-01-30 11:51:45 +0100

    working on getAllTasks()

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore} from '@angular/fire/firestore'
+import { AngularFirestore} from '@angular/fire/firestore';
+import { Data } from '@angular/router';
+
 
 
 @Injectable({
@@ -14,10 +16,28 @@ export class DatabaseService {
     return true
   }
 
-  getAllTasks(): Array<object>{
-    this.afFirestore.collection('task').get().subscribe
+  getAllTasks(): Array<string>{
+  let tasks = []
+    this.afFirestore.collection('task').get().subscribe((snapshot) => {
+      snapshot.docs.forEach(tasks => {
+        tasks.data
+      });
+    })
+    return tasks
   }
-
+/*
+  this.afDB.list('Tasks/').snapshotChanges(['child_added', 'child_removed']).subscribe(actions => {
+    this.tasks = [];
+    actions.forEach(action => {
+      this.tasks.push({
+        key: action.key,
+        title: action.payload.exportVal().title,
+        description: action.payload.exportVal().description,
+        date: action.payload.exportVal().date.substring(11, 16),
+      });
+    });
+  });
+*/
   
 /*
   getTask(taskId: string): object {
